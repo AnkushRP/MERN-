cfb97e759f61a17008298169d513ae6d0ac5e1d2
AnkushRP <147685188+AnkushRP@users.noreply.github.com>
2025-10-23 17:11:46 +0530

    Add files via upload

---
+++ Other/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+import Card from './Card';
+
+const Card = ({ title, content }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Card;
