58198de310c6f3277211d706e421e8160c4dd872
AnkushRP <147685188+AnkushRP@users.noreply.github.com>
2025-12-09 13:20:56 +0530

    Fix server.js formatting and connection string

--- internship week-3/week3/server.js
+++ internship week-3/week3/server.js
@@ -10,7 +10,7 @@ app.use("/",routes);
 //Db connection
 mongoose
 .connect(
-    "mongodb+srv://ankushrp2004:ankush123@cluster0.nvbesvi.mongodb.net/internship-batch3"
+    "mongodb+srv://@cluster0.nvbesvi.mongodb.net/internship-batch3"
 )
 .then(()=>{
     console.log("Database is connected successfully");
@@ -25,4 +25,5 @@ app.get("/test",(req,res)=>{
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
+
 });
